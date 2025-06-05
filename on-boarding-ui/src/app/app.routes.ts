bdf16583f0650d5938a9333098da9bec96351f72
Patha Alok <patha.alok@73strings.com>
2025-06-05 13:02:11 +0530

    manual UI changes

--- on-boarding-ui/src/app/app.routes.ts
+++ on-boarding-ui/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { VerifyQuestionareComponent } from './verify-questionare/verify-questionare.component';
 import { OnBoardQuestionareComponent } from './on-board-questionare/on-board-questionare.component';
 import { SubmitQuestionaresComponent } from './submit-questionares/submit-questionares.component';
+import { OnBoardQuestionareManualComponent } from './on-board-questionare-manual/on-board-questionare-manual.component';
 
 export const routes: Routes = [
-    { path: 'on-board-questionare', component: OnBoardQuestionareComponent },
+  { path: 'on-board-questionare', component: OnBoardQuestionareManualComponent },
+    { path: 'on-board-questionare-agent', component: OnBoardQuestionareComponent },
     { path: 'verify-questionare', component: VerifyQuestionareComponent },
   { path: 'submit-questionare', component: SubmitQuestionaresComponent },
   { path: '**', redirectTo: 'on-board-questionare' } 
---
+++ on-boarding-ui/src/app/on-board-questionare-manual/on-board-questionare-manual.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-on-board-questionare-manual',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './on-board-questionare-manual.component.html',
+  styleUrls: ['./on-board-questionare-manual.component.scss'],
+})
+export class  OnBoardQuestionareManualComponent implements OnInit {
+  sessionId = '';
+  messages: { role: string, text: string }[] = [];
+  userInput = '';
+  previewMode = false;
+  previewAnswers: any[] = [];
+  answerHistory: string[][] = [];
+  editingIndex: number | null = null;
+  finalJson: any = null;
+  confirmed = false;
+  startNewOnboarding = false;
+
+  questions: string[] = [
+    "Enter On Boarding Name",
+    "Enter Partition ? (P0, P1, P2, P3, P4, P5)",
+    "Enter Eligible SOR Codes ? (Example: ACCT/SOR,DEAL/SOR)",
+    "Enter BUS UNIT",
+    "Enter RCC RULES",
+    "Enter Sampling Rule Ref",
+    'Enter Sampling Id',
+    'Enter Sampling Data'
+  ];
+
+  formGroup!: FormGroup ;
+
+  constructor(private fb: FormBuilder,private http: HttpClient) {}
+
+  ngOnInit() {
+    const controlsConfig = this.questions.reduce((acc, _, i) => {
+      acc['field' + i] = ['', Validators.required]; 
+      return acc;
+    }, {} as { [key: string]: any });
+  
+    this.formGroup = this.fb.group(controlsConfig);
+  }
+
+  onSubmit() {
+    const answers: { [key: string]: string } = {};
+    Object.keys(this.formGroup.controls).forEach((key, index) => {
+      answers[index.toString()] = this.formGroup.get(key)?.value || '';
+    });
+  
+    this.finalJson = {
+      questions: this.questions,
+      answers: answers
+    };
+  
+    this.sendDataForVerification();
+  }
+
+  sendDataForVerification(){
+    this.http.post<any>('http://localhost:8000/store_verify_qa', this.finalJson).subscribe(res => {
+      this.confirmed = true;
+    });
+  }
+
+
+
+}
+
+  
